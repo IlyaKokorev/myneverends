ca4c75353bbe9ea839453372af227840fb5ac244
ilya.kok <ilya.kok@okwork.io>
2021-08-09 17:35:56 +0300

    f

--- app/controllers/compliments_controller.rb
+++ app/controllers/compliments_controller.rb
@@ -2,8 +2,8 @@
 
 class ComplimentsController < ApplicationController
   def index
-    sincom = [Sincom.all]
-    dubcom = [Precom.all, Pastcom]
+    sincom = [Sincom.all.sample.title]
+    dubcom = [Precom.all.sample.title, Pastcom.all.sample.title]
 
     @compliment = [sincom, dubcom].sample(1)
   end
